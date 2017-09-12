e37fce9b93fc0a1f5a7cb3e1e75b25219e51d0a3
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-12 09:53:35 -0300

    add programação da index

--- livros/index.php
+++ livros/index.php
@@ -1,10 +1,29 @@
 <?php
+
 /**
  * Example Application
  *
  * @package Example-application
  */
 $titulo = "Livros da Natec Editora";
-require '../libs/Smarty.class.php';
+$array_livros = array(
+    "Desenvolvendo com Laravel",
+    "Entendendo Algoritmos",
+    "Desenvolvendo para iPhone e iPad - 5ª edição",
+    "Introdução à Orientação a Objetos com C++ e Python",
+    "Laravel para ninjas"
+);
+$array_autores = array(
+    "Matt Stauffer",
+    "Aditya Y. Bhargava",
+    "Ricardo Lecheta",
+    "Orlando Saraiva Jr.",
+    "Ademir C. Gabardo"
+);
 
+require '../libs/Smarty.class.php';
 $smarty = new Smarty;
+$smarty->assign("titulo", $titulo);
+$smarty->assign("livros", $array_livros);
+$smarty->assign("autores", $array_autores);
+$smarty->assign("index.tpl");
